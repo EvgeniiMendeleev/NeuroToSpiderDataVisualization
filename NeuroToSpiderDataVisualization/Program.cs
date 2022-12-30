5db83bd360fb296395800a92584277fc14730916
EvgenyMendeleev <43094600+EvgeniiMendeleev@users.noreply.github.com>
2022-12-30 18:45:00 +0300

    Добавьте файлы проекта.

---
+++ NeuroToSpiderDataVisualization/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace NeuroToSpiderDataVisualization
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}

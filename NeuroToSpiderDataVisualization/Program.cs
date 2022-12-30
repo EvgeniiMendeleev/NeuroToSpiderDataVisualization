4b07276a48a661332c2f56ac1993da2942e4cf1d
EvgenyMendeleev <43094600+EvgeniiMendeleev@users.noreply.github.com>
2022-12-30 18:56:39 +0300

    Инициализация проекта - Создан примерный код работы программы.

--- NeuroToSpiderDataVisualization/Program.cs
+++ NeuroToSpiderDataVisualization/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.IO.Ports;
 
 namespace NeuroToSpiderDataVisualization
 {
@@ -6,7 +7,37 @@ namespace NeuroToSpiderDataVisualization
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("COM - порт, на котором расположен паук:");
+            string spiderComPort = Console.ReadLine();
+
+            Console.WriteLine("COM - порт, на котором расположен нейроинтерфейс: ");
+            string neurointerfaceComPort = Console.ReadLine();
+
+            SerialPort spiderPort = new SerialPort(spiderComPort);
+            //TODO: Можно попробовать подключить мой изначальный класс из проекта NeuroVR для считывания данных с нейроинтерфейса.
+            SerialPort neurointerfacePort = new SerialPort(neurointerfaceComPort);
+
+            //TODO: Настроить COM - порты.
+
+            spiderPort.Open();
+            neurointerfacePort.Open();
+
+            try
+            {
+                while (true)
+                {
+                    //TODO: Реализовать считывание с нейроинтерфейса.
+                    Console.WriteLine("Здесь выводятся данные с нейроинтерфейса!");
+                    //TODO: Реализовать отправку данных на паука.
+                }
+            }
+            catch (Exception exp)
+            {
+                Console.WriteLine("(ОШИБКА ВО ВРЕМЯ РАБОТЫ!) " + exp.Message);
+            }
+
+            spiderPort.Close();
+            neurointerfacePort.Close();
         }
     }
 }

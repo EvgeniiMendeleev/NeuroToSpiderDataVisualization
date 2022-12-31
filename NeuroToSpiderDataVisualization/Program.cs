54d11a13997ee05c73ede4124d2234674f7996db
EvgenyMendeleev <43094600+EvgeniiMendeleev@users.noreply.github.com>
2022-12-31 10:37:25 +0300

    Первая версия программы - сделана программа по отображению данных, пересылаемых с нейроинтерфейса пауку.

--- NeuroToSpiderDataVisualization/Program.cs
+++ NeuroToSpiderDataVisualization/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.IO.Ports;
 
 namespace NeuroToSpiderDataVisualization
@@ -8,27 +9,42 @@ namespace NeuroToSpiderDataVisualization
         static void Main(string[] args)
         {
             Console.WriteLine("COM - порт, на котором расположен паук:");
-            string spiderComPort = Console.ReadLine();
+            SerialPort spiderPort = new SerialPort(Console.ReadLine());
+            spiderPort.Open();
 
             Console.WriteLine("COM - порт, на котором расположен нейроинтерфейс: ");
-            string neurointerfaceComPort = Console.ReadLine();
-
-            SerialPort spiderPort = new SerialPort(spiderComPort);
-            //TODO: Можно попробовать подключить мой изначальный класс из проекта NeuroVR для считывания данных с нейроинтерфейса.
-            SerialPort neurointerfacePort = new SerialPort(neurointerfaceComPort);
-
-            //TODO: Настроить COM - порты.
-
-            spiderPort.Open();
+            SerialPort neurointerfacePort = new SerialPort(Console.ReadLine());
             neurointerfacePort.Open();
 
             try
             {
                 while (true)
                 {
-                    //TODO: Реализовать считывание с нейроинтерфейса.
-                    Console.WriteLine("Здесь выводятся данные с нейроинтерфейса!");
-                    //TODO: Реализовать отправку данных на паука.
+                    int sync1, sync2;
+                    do
+                    {
+                        sync1 = neurointerfacePort.ReadByte();
+                        sync2 = neurointerfacePort.ReadByte();
+                    } while (sync1 != 170 && sync2 != 170);
+                    
+                    int pLength = neurointerfacePort.ReadByte();
+                    
+                    byte[] bytes = new byte[pLength];
+                    neurointerfacePort.Read(bytes, 0, pLength);
+
+                    foreach (byte val in bytes) Console.Write(val + " ");
+                    Console.WriteLine();
+
+                    int checkSum = neurointerfacePort.ReadByte();
+
+                    List<byte> spiderData = new List<byte>();
+                    spiderData.Add(Convert.ToByte(sync1));
+                    spiderData.Add(Convert.ToByte(sync2));
+                    spiderData.Add(Convert.ToByte(pLength));
+                    foreach (byte val in bytes) spiderData.Add(val);
+                    spiderData.Add(Convert.ToByte(checkSum));
+
+                    spiderPort.Write(spiderData.ToArray(), 0, spiderData.Count);
                 }
             }
             catch (Exception exp)
